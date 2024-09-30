4f0d0b36490f24a2780fe1f54c6d263e30777bae
Loner6671 <donghua6671@outlook.com>
2024-09-30 14:41:32 +0800

    main

--- study.cpp
+++ study.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
+void simon(int);
 int main()
 {
     using namespace std;
-    cout<<"LDX";
+    simon(3);    //赋予Simon一个值
+    cout<<"please tap in a number: ";  
+    int cut;
+    cin>>cut;
+    simon(cut);  //再次赋予Simon一i个值
+    cout<<"Done!"<<endl;
+    
+}
+void simon(int n)  //将赋予Simon的值赋予n
+{
+    using namespace std;
+    cout<<"He says "<<n<<" times hello"<<endl;
+
 }

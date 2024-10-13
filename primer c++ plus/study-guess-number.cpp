9735e3f15987216e6694cfc039b4731bcb3da4cc
Loner6671 <donghua6671@outlook.com>
2024-10-13 20:03:50 +0800

    main

--- primer c++ plus/study-guess-number.cpp
+++ primer c++ plus/study-guess-number.cpp
@@ -6,10 +6,11 @@ int main(void)
     using namespace std;
     srand((unsigned int)time(NULL));
     int fac=rand()%100+1;
-    int num=0;
+    int num,n=0;
     string fis="恭喜你";
     string sec="小了";
     string thi="大了";
+    string gg="再练练吧";
     while(num!=fac)
     {
         cin>>num;
@@ -19,6 +20,10 @@ int main(void)
             cout<<sec<<endl;
         else 
             cout<<fis<<endl;
+        n+=1;
+        if(n>8)
+            {cout<<"fw"<<gg<<endl;
+            break;}
     }
 system("pause");
 }

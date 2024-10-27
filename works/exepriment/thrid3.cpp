bea3c31bca4feeb3d7a9b92fef544a0f9680e718
Loner6671 <donghua6671@outlook.com>
2024-10-27 19:42:57 +0800

    main

---
+++ works/exepriment/thrid3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int su(int n,int m);
+void search();
+int main(void)
+{
+    cout<<"Please tap in min number:"<<endl;
+    int min,max;
+    cin>>min;
+    cout<<"Please tap in a max number:"<<endl;
+    cin>>max;
+    su(min,max);
+
+}
+void search()
+{
+    int n;
+    for(int i=1;i<50000;i++)
+        if(i%n==0)
+            
+}
+int su(int n,int m)
+{
+    for(int i=n;i<=m;i++)
+
+
+}

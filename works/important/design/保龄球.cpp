b25b87b40f6354ebfb96413d61b5d52af8a20bce
Loner6671 <donghua6671@outlook.com>
2024-12-14 16:52:20 +0800

    main

---
+++ works/important/design/保龄球.cpp
@@ -0,0 +1,135 @@
+#include<iostream>
+#include<cstring>
+#include<fstream>
+#include<ctime>
+using namespace std;
+int sum()
+{
+    static bool seeded=false;
+    if (!seeded) 
+    {
+        srand((unsigned int)time(NULL));
+        seeded = true;
+    }
+    int ball[20]={0};
+    int score[10]={0};
+    int num1=0,num2=0,sum=0;
+    for(int i=0;i<9;i++)
+    {
+        num1=0+rand()%10;
+        if(i==0)
+        {
+            ball[0]=num1;
+            if(num1!=10)
+            {
+                num2=0+rand()%num1;
+                ball[1]=num2;
+            }
+            else
+            {
+                ball[1]=0;
+            }
+        }
+        else
+        {
+            if(num1!=10)
+            {
+                num2=0+rand()%(10-num1);
+                ball[i*2]=num1;
+                ball[i*2+1]=num2;
+            }
+            else
+            {
+                ball[i*2]=10;
+                ball[i*2+1]=0;
+            }
+        }
+    }
+    ball[17]=0+rand()%10;
+    if(ball[17]==10)
+    {
+        ball[18]=0;
+        ball[19]=0+rand()%10;
+        ball[20]=0+rand()%10;
+        score[9]=ball[17]+ball[18]+ball[19]+ball[20];
+    }
+    else
+    {
+        ball[18]=0+rand()%(10-ball[17]);
+        if(ball[17]+ball[18]==10)
+        {
+            ball[19]=0+rand()%10;
+            score[9]=ball[17]+ball[18]+ball[19];
+        }
+        else
+        {
+            score[9]=ball[17]+ball[18];
+        }
+    }
+    for(int j=0;j<9;j++)        
+    {
+        if(j==0)
+        {
+            if(ball[0]!=10 && ball[0]+ball[1]!=10)
+            {
+                score[0]=ball[0]+ball[1];
+            }
+            else if(ball[0]!=10 && ball[0]+ball[1]==10)
+            {
+                score[0]=ball[0]+ball[1]+ball[2];
+            }
+            else if(ball[0]==10)
+            {
+                if(ball[2]!=10)
+                {
+                score[0]=ball[0]+ball[1]+ball[2]+ball[3];
+                }
+                else
+                {
+                    score[0]=ball[0]+ball[1]+ball[2]+ball[3]+ball[4];
+                }
+            }
+        }
+        else
+        {
+            if(ball[j*2]!=10 && ball[j*2]+ball[j*2+1]!=10)
+            {
+                score[j]=ball[j*2]+ball[j*2+1];
+            }
+            else if(ball[j*2]!=10 && ball[j*2]+ball[j*2+1]==10)
+            {
+                score[j]=ball[j*2]+ball[j*2+1]+ball[j*2+2];
+            }
+            else if(ball[j*2]==10)
+            {
+                if(ball[j*2+2]!=10)
+                {
+                    score[j]=ball[j*2]+ball[j*2+1]+ball[j*2+2]+ball[j*2+3];
+                }
+                else
+                {
+                    score[j]=ball[j*2]+ball[j*2+1]+ball[j*2+2]+ball[j*2+3]+ball[j*2+4];
+                }
+            }
+        }
+    }
+    for(int k=0;k<10;k++)
+    {
+        sum+=score[k];
+    }
+    return sum;
+}
+int main()
+{
+    int summary[9];
+    for(int i=0;i<10;i++)
+    {
+        summary[i]=sum();
+    }
+    char name[10]={'a','b','c','d','e','f','g','h','i','j'};
+    for(int j=0;j<10;j++)
+    {
+        cout<<name[j]<<" "<<summary[j]<<endl;
+    }
+    system("pause");
+}
